1547cf6310dc25f550c508ccf04c33db0883bfb2
Gianluca200600 <gianlucaerbetta00@gmail.com>
2025-06-10 21:04:07 +0200

    Initial Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Games } from './games/games';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'games', component: Games },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
