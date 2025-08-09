2a236cb1537c4e9898cc779c142e7b56bb09e05a
Gianluca200600 <gianlucaerbetta00@gmail.com>
2025-08-09 11:23:30 +0200

    Game Page

--- src/app/game.ts
+++ src/app/game.ts
@@ -3,15 +3,21 @@ import { ActivatedRoute } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { GameService } from './model/game-service';
 import { mergeMap } from 'rxjs';
+import { faClock, faUser, faTags, faCogs } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './game.html',
   styleUrl: './game.css'
 })
 export class Game {
   
+  faClock = faClock;
+  faUser = faUser;
+  faTags = faTags;
+  faCogs = faCogs;
   params$ = inject(ActivatedRoute).params;
   gamesService = inject(GameService);
   
