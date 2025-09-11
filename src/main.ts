acaaa336efc8077d0792bbb6de0de9fa8fe031a9
Gianluca200600 <gianlucaerbetta00@gmail.com>
2025-09-11 20:57:02 +0200

    ShadowDOM

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { createApplication } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+createApplication({ providers: [] }).then(appRef => {
+  const injector = appRef.injector;
+  const element = createCustomElement(App, { injector });
+  customElements.define('my-ng-app', element);
+});
